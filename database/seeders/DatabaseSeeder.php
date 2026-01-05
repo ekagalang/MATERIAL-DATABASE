01caf8cc9ac8c617a891edff47ee6cd974918e95
Eka Galang <ekagalangpratama@gmail.com>
2026-01-05 14:52:08 +0700

    Fix Material setting for Ceramic

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,10 +10,12 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UnitSeeder::class,
+            MaterialSettingSeeder::class, // HARUS DI AWAL sebelum material seeders
             CatSeeder::class,
             BrickSeeder::class,
             CementSeeder::class,
             SandSeeder::class,
+            CeramicSeeder::class,
             BrickInstallationTypeSeeder::class,
             MortarFormulaSeeder::class,
 
---
+++ database/seeders/MaterialSettingSeeder.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\MaterialSetting;
+
+class MaterialSettingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $materials = [
+            [
+                'material_type' => 'brick',
+                'is_visible' => true,
+                'display_order' => 1,
+            ],
+            [
+                'material_type' => 'cat',
+                'is_visible' => true,
+                'display_order' => 2,
+            ],
+            [
+                'material_type' => 'cement',
+                'is_visible' => true,
+                'display_order' => 3,
+            ],
+            [
+                'material_type' => 'sand',
+                'is_visible' => true,
+                'display_order' => 4,
+            ],
+            [
+                'material_type' => 'ceramic',
+                'is_visible' => true,
+                'display_order' => 5,
+            ],
+        ];
+
+        foreach ($materials as $material) {
+            MaterialSetting::updateOrCreate(
+                ['material_type' => $material['material_type']],
+                $material
+            );
+        }
+
+        $this->command->info('MaterialSettings seeded successfully!');
+    }
+}
